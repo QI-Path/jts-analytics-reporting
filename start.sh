d5e11feaa13cc08e9ac20d7228b3e8f60cbafcd7
Ryan McKeel <ryan@ryanmckeel.com>
2025-11-17 16:31:42 -0700

    way to customize the superset config, starting with branding

---
+++ start.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# https://medium.com/%40supreethmc/superset-customization-of-the-logo-within-a-docker-container-eccff32a6e39
+
+docker compose -f docker-compose-image-tag.yml up --remove-orphans
+
